1116c88b76d815f7445e82afcc60ce99d385a247
SadboiTay <tg.hakes@gmail.com>
2021-12-04 17:22:23 -0700

    begin contact section styling

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,9 +1,68 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { validateEmail } from '../../assets/utils/helpers';
 
-function Contact() {
+function ContactForm() {
+    const [formState, setFormState] = useState({ name: '', email: '', message: '' });
+    const { name, email, message } = formState;
+    const [errorMessage, setErrorMessage] = useState('');
+
+    function handleChange(e) {
+        if (e.target.name === 'email') {
+            const isValid = validateEmail(e.target.value);
+
+            if (!isValid) {
+                setErrorMessage('Your email is invalid.');
+            } else {
+                setErrorMessage('');
+            }
+        } else {
+            if (!e.target.value.length) {
+                setErrorMessage(`${e.target.name} is required.`);
+            } else {
+                setErrorMessage('');
+            }
+        }
+
+        if (!errorMessage) {
+            setFormState({ ...formState, [e.target.name]: e.target.value });
+        }
+    }
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        console.log(formState);
+    }
+
     return (
-        <h3>CONTACT ME BITCH</h3>
+        <section id="contact-section">
+            <h2>Feel free to shoot me an email:</h2>
+            <div className="email-block">
+                <a className="email-address" href="mailto:hello@taylorhakes.dev">hello@</a><br />
+                <a className ="email-address email-domain" href="mailto:hello@taylorhakes.dev">taylorhakes.dev</a>
+            </div>
+            <h2>Or, drop in a message here:</h2>
+            <form id="contact-form" onSubmit={handleSubmit}>
+                <div>
+                    <label htmlFor="name">Name:</label>
+                    <input type="text" defaultValue={name} onBlur={handleChange} name="name" />
+                </div>
+                <div>
+                    <label htmlFor="email">Email address:</label>
+                    <input type="email" defaultValue={email} onBlur={handleChange} name="email" />
+                </div>
+                <div>
+                    <label htmlFor="message">Message:</label>
+                    <textarea name="message" defaultValue={message} onBlur={handleChange} rows="5" />
+                </div>
+                {errorMessage && (
+                    <div>
+                        <p className="error-text">{errorMessage}</p>
+                    </div>
+                )}
+                <button type="submit">Submit</button>
+            </form>
+        </section>
     )
 }
 
-export default Contact;
+export default ContactForm;
