d500e9031eb665cc26d41595d6d80babd27f86cc
Taylor Hakes <86327207+SadboiTay@users.noreply.github.com>
2021-12-11 13:22:08 -0700

    fix homepage router load

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ function App() {
         />
         <div className="container">
           <Switch>
-            <Route exact path="/" component={About} />
+            <Route exact path="/react-portfolio" component={About} />
             <Route exact path="/about" component={About} />
             <Route exact path="/portfolio" component={Portfolio} />
             <Route exact path="/contact" component={Contact} />
