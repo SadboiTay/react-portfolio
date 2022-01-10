f80539b957e2719076de55da13f469beb1de853f
Taylor Hakes <86327207+SadboiTay@users.noreply.github.com>
2022-01-10 11:02:39 -0700

    add production environment server line

--- server/server.js
+++ server/server.js
@@ -7,6 +7,10 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/build')));
+}
+
 app.get('*', (req, res) => {
     console.log('sending index.html')
     res.sendFile(path.join(__dirname, '../client/build/index.html'));
